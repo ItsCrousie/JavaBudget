e72ffb8e2bcbee09e99c0abd3531fda7df7a2407
Brian Crouse <itscrousie@gmail.com>
2025-06-05 14:45:17 -0400

    FIX: Argument Exceptions

--- src/Debit.java
+++ src/Debit.java
@@ -1,4 +1,4 @@
-public class Debit () {
+public class Debit {
     private String name;
     private double value;
 
@@ -7,11 +7,29 @@ public class Debit () {
         this.value = value;
     }
     
-    public double getName () {
+    public String getName () {
         return name;
     }
 
     public double getValue () {
         return value;
     }
+
+    public void setName (String name) {
+        if (name.contains("^[a-zA-z\\s]")) {
+            throw new IllegalArgumentException("Name must only contain letters and whitespace.");
+        }
+        this.name = name;
+    }
+
+    public void setValue (double value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("Value must be greater than $0.");
+        }
+        this.value = value;
+    }
+
+    public String displayDebitInfo () {
+        return "Name: " + getName() + " Value: $" + getValue();
+    } 
 }
