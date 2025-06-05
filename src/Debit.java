82ed764231fe03ed214dd9e82e69ee677b3fc974
Brian Crouse <itscrousie@gmail.com>
2025-06-05 14:48:19 -0400

    FIX: Constructor

--- src/Debit.java
+++ src/Debit.java
@@ -3,7 +3,16 @@ public class Debit {
     private double value;
 
     public Debit (String name, double value) {
+        //Only allow letters and whitespace in names.
+        if (name.contains("^[a-zA-z\\s]")) {
+            throw new IllegalArgumentException("Name must only contain letters and whitespace.");
+        }
         this.name = name;
+        //Don't allow less than 0 values because they don't add anything.
+        if (value <= 0) {
+            throw new IllegalArgumentException("Value must be greater than $0.");
+        }
+
         this.value = value;
     }
     
@@ -23,9 +32,6 @@ public class Debit {
     }
 
     public void setValue (double value) {
-        if (value <= 0) {
-            throw new IllegalArgumentException("Value must be greater than $0.");
-        }
         this.value = value;
     }
 
