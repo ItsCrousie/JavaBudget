dd3c35d707507e6b7da163eee4c6a415f17e3518
Brian Crouse <itscrousie@gmail.com>
2025-06-05 16:22:20 -0400

    FIX: Dry constructor and input validation on setters

--- src/Debit.java
+++ src/Debit.java
@@ -1,37 +1,39 @@
+
+import java.math.BigDecimal;
+
 public class Debit {
     private String name;
-    private double value;
+    private BigDecimal value;
 
-    public Debit (String name, double value) {
-        //Only allow letters and whitespace in names.
-        if (name.contains("^[a-zA-z\\s]")) {
-            throw new IllegalArgumentException("Name must only contain letters and whitespace.");
-        }
-        this.name = name;
-        //Don't allow less than 0 values because they don't add anything.
-        if (value <= 0) {
-            throw new IllegalArgumentException("Value must be greater than $0.");
-        }
-
-        this.value = value;
+    public Debit (String name, BigDecimal value) {
+        setName(name);
+        setValue(value);
     }
     
     public String getName () {
         return name;
     }
 
-    public double getValue () {
+    public BigDecimal getValue () {
         return value;
     }
 
     public void setName (String name) {
-        if (!name.contains("[a-zA-z\\s]+")) {
+        if (!name.matches("[a-zA-Z\\s]+")) {
             throw new IllegalArgumentException("Name must only contain letters and whitespace.");
         }
         this.name = name;
     }
 
-    public void setValue (double value) {
+    public void setValue (BigDecimal value) {
+        //Check if value is greater than 0
+        if (value.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Value must be positive and greater than 0."); 
+        }
+        //Check if value is rounded to two decimal places
+        if (value.scale() > 2) {
+            throw new IllegalArgumentException("Value must be rounded to nearest cent");
+        }
         this.value = value;
     }
 
